db6006231e0215e4fb1bf5038168793f07236c6e
Harly <harlycarreiro@gmail.com>
2018-12-18 16:30:56 -0300

    first app 181219

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MovieProvider } from '../../providers/movie/movie';
+
 
 /**
  * Generated class for the FeedPage page.
@@ -12,13 +14,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html',
+  providers: [
+    MovieProvider
+  ]
 })
 export class FeedPage {
+  public objetoFeed = {
+      titulo:"Harly Carreiro",
+      data: "November 5, 1955",
+      descricao:"Estou criando um app incrível...",
+      qtdLikes:12,
+      qtdComments:4,
+      timeComment:"11h ago"
+  }
+
+  public listaFilmes = new Array<any>();
 
   //string, number, any
   public nomeUsuario:string = "Harly Carreiro - Código";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private movieProvider: MovieProvider
+    ) {
   }
 
   public somaDoisNumeros(num1:number, num2:number): void {
@@ -27,6 +46,25 @@ export class FeedPage {
 
   ionViewDidLoad() {
     //this.somaDoisNumeros(10, 99)
+    this.movieProvider.getLatestMovies().subscribe(
+      data=>{
+
+/*          //transforma a resposta em um obj qualquer tipo
+        const response = (data as any);
+        //transforma em JSON
+        const objetoRetorno = JSON.parse(response._body); 
+
+        this.listaFilmes = objetoRetorno.results;
+        console.log((objetoRetorno)); */
+
+
+        this.listaFilmes = (data as any).results;
+        console.log((data));  
+      },
+      error=>{
+        console.log(error);
+      }
+    )
   }
 
 }
